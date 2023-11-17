c790d8c828d9ac8b4c128d8acc894ff821758bba
Baggu516 <bhargavsainallagutla@gmail.com>
2023-11-17 19:05:00 +0530

    type-script-todo

--- my-app/src/components/Todo.tsx
+++ my-app/src/components/Todo.tsx
@@ -3,20 +3,50 @@ import TodoItem from './TodoItem'
 const Todo = () => {
     const [a,setA]=useState<string>("")
     const [arr,setArr]=useState<string[]>([])
+    const [h,setH]=useState<boolean>(true)
+    const [index,setIndex]=useState<number>(0)
+    const [s,setS]=useState<number[]>([])
     const HandleClick=()=>{
         let t:string[]=[...arr,a]
         console.log(t)
         setArr(t)
+        setA("")
 
+    }
+    const HandleDelete=(x:number)=>{
+       let t:string[]=[...arr]
+       t.splice(x,1)
+       setArr(t)
+    }
+    // .............edit......
+    const HandleEdit=(c:number)=>{
+      setA(arr[c])
+      setH(false)
+      setIndex(c)
+
+
+    }
+    // ....
+    const HandleUpdate=()=>{
+      let t:string[]=[...arr]
+      t.splice(index,1,a)
+      setArr(t)
+      setH(true)
+      setA("")
+    }
+    // ............
+    const HandleDone=(y:number)=>{
+        setS([...s,y])
     }
     console.log("arr",arr)
   return (
     <div className="container">
-        <input type="text" onChange={(e)=>setA(e.target.value)} value={a}/>
-        <button  onClick={HandleClick}>Add</button>
+      <h1>Todo List</h1>
+        <input type="text" onChange={(e)=>setA(e.target.value)} value={a} placeholder='type....'/>
+        {(h==true)?<button className='btn' onClick={HandleClick}>Add</button>:<button className='btn' onClick={HandleUpdate}>Update</button>}
         {arr&&arr.map((item,i)=>{
             return(
-                 <TodoItem item={item} key={i} />
+                 <TodoItem s={s} HandleDone={HandleDone} item={item} key={i} index={i} HandleDelete={HandleDelete} HandleEdit={HandleEdit} h={h} HandleUpdate={HandleUpdate}/>
             )
             
         })}
--- my-app/src/components/TodoItem.tsx
+++ my-app/src/components/TodoItem.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 type TodoItemprops={
-    item:string
+    item:string,
+    HandleDelete:(x:number)=>void,
+    index:number,
+    HandleEdit:(c:number)=>void,
+    h:boolean,
+    HandleUpdate:(c:number)=>void,
+    s:number[],
+    HandleDone:(f:number)=>void
 }
-const TodoItem = ({item}:TodoItemprops) => {
+const TodoItem = ({item,HandleDelete,index,HandleEdit,h,HandleUpdate,s,HandleDone}:TodoItemprops) => {
   return (
     <div className="Todo-Item">
-        <p>{item}</p>
+        {!s.includes(index)?<p>{item}</p>:<p><s>{item}</s></p>}
+        <div>
+        <button onClick={(e)=>HandleDelete(index)}>Delete</button>
+        {<button onClick={(e)=>HandleEdit(index)}>Edit</button>}
+        <button onClick={()=>HandleDone(index)}>Done</button>
+        </div>
+       
+        {/* <button>Done</button> */}
         {/* <ion-icon name="create-outline"></ion-icon> */}
     </div>
   )
